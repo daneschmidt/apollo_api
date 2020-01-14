8f651d549ef61a1938969071c9dbae2925588c8f
Dane Schmidt <daneschmidt@gmail.com>
2020-01-14 13:24:25 -0600

    get is setup for missions and working correctly

---
+++ server/routes/mission.router.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/', (req, res) => {
+    
+    let queryText =
+    `SELECT * FROM "mission"
+    JOIN "launch_info" ON "mission"."launch_id" = "launch_info"."launch_id"
+    JOIN "landing_info" ON "mission"."landing_id" = "landing_info"."landing_id"
+    JOIN "crew" ON "mission"."crew_id" = "crew"."crew_id"
+    JOIN "orbital_info" ON "mission"."orbit_id" = "orbital_info"."orbit_id"
+    JOIN "lunar_info" ON "mission"."lunar_id" = "lunar_info"."lunar_id"
+    ORDER BY "mission_name" DESC;`;
+
+    pool.query(queryText)
+        .then(results => {
+            res.send(results.rows);
+        })
+        .catch(error => {
+            console.log(`couldn't get movie`, error);
+            res.sendStatus(500);
+        })
+
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const missionRouter = require('../server/routes/mission.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/mission', missionRouter);
 
 // Serve static files
 app.use(express.static('build'));
