426bc96df979fb859873c164f70951e0ca0c2454
Dane Schmidt <daneschmidt@gmail.com>
2020-01-14 14:13:34 -0600

    crew router setup and tested

---
+++ server/routes/crew.router.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/', (req, res) => {
+    
+    let queryText =
+    `SELECT * FROM "mission"
+    JOIN "crew" ON "mission"."crew_id" = "crew"."crew_id"
+    ORDER BY "mission_name" DESC;`;
+
+    pool.query(queryText)
+        .then(results => {
+            res.send(results.rows);
+        })
+        .catch(error => {
+            console.log(`couldn't get movie`, error);
+            res.sendStatus(500);
+        })
+
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const passport = require('./strategies/user.strategy');
 const userRouter = require('./routes/user.router');
 const missionRouter = require('../server/routes/mission.router');
 const launchRouter = require('../server/routes/launch.router');
+const crewRouter = require('../server/routes/crew.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -28,6 +29,7 @@ app.use(passport.session());
 app.use('/api/user', userRouter);
 app.use('/api/mission', missionRouter);
 app.use('/api/launch_info', launchRouter);
+app.use('/api/crew_info', crewRouter);
 
 // Serve static files
 app.use(express.static('build'));
