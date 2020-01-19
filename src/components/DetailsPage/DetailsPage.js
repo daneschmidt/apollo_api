d2436243cebbaec45c0fddff588b5e2898c2d7dc
Dane Schmidt <daneschmidt@gmail.com>
2020-01-19 10:18:36 -0600

    got details page returning single misison info

--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -18,8 +18,54 @@ class DetailsPage extends Component {
         const detailsArray = this.props.store.specMissionReducer.map((item, index) => {
             return (
                 <div key={index}>
-                    {item.mission_name}
-                </div>
+          <img src={item.mission_patch} alt="mission patch" width="200"></img>
+
+          <div>
+          <h3>Mission Name:</h3> {item.mission_name}<br></br>
+          <h5>Mission Objectives:</h5>
+          <pre>
+          {item.mission_objectives}<br></br>
+          <h3>Mission Highlights:</h3>
+          {item.mission_highlights}<br></br>
+          <br></br>
+          </pre>
+          </div>
+
+          <h3>Crew</h3>
+          <div><pre>
+          Commander: {item.commander}<br></br>
+          Lunar Module Pilot: {item.lunar_module_pilot}<br></br>
+          Command Module Pilot: {item.command_module_pilot}<br></br>
+          <br></br>
+          Backup Commander: {item.backup_commander}<br></br>
+          Backup Lunar Module Pilot: {item.backup_lunar_module_pilot}<br></br>
+          Backup Command Module Pilot: {item.backup_command_module_pilot}<br></br>
+          </pre></div>
+          <br></br>
+          <h3>Launch</h3>
+          Launch Info:<br></br>
+          Launch Date: {item.launch_date}<br></br>
+          Launch Location: {item.location}<br></br>
+          Payload: {item.payload}<br></br>
+          <br></br>
+          <h3>Orbit</h3>
+          Altitude: {item.altitude}<br></br>
+          Inclination: {item.inclination}<br></br>
+          Number of Orbits: {item.number_of_orbits}<br></br>
+          Mission Duration: {item.mission_duration}<br></br>
+          Distance Traveled: {item.distance_traveled}<br></br>
+          <br></br>
+          <h3>Lunar</h3>
+          Lunar Location: {item.lunar_location}<br></br>
+          Lunar Coordinates: {item.lunar_coordinates}<br></br>
+          Time on Surface: {item.time_on_lunar_surface}<br></br>
+          <br></br>
+          <h3>Landing</h3>
+          Landing Date: {item.landing_date}<br></br>
+          Landing Location: {item.landing_location}<br></br>
+          Recovery Ship: {item.recovery_ship}<br></br>
+          <br></br>
+        </div>
             )
         })
 
