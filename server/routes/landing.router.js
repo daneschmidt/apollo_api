d3766f7cbbdc45540c56d2babda0b749f86ac8cc
Dane Schmidt <daneschmidt@gmail.com>
2020-01-20 14:10:29 -0600

    setup landing router because apparently I missed it before

---
+++ server/routes/landing.router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/', (req, res) => {
+    
+    let queryText =
+    `SELECT * FROM "landing_info"
+    ORDER BY "landing_id" DESC;`;
+
+    pool.query(queryText)
+        .then(results => {
+            res.send(results.rows);
+        })
+        .catch(error => {
+            console.log(`couldn't get data`, error);
+            res.sendStatus(500);
+        })
+
+});
+
+
+module.exports = router;
