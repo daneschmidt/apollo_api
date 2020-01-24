9b546d58ae88c9aaa9899a351fe4a29894681c77
Dane Schmidt <daneschmidt@gmail.com>
2020-01-24 09:12:38 -0600

    got crewPic router updated to the new standard requiring API key to access

--- server/routes/crewPic.router.js
+++ server/routes/crewPic.router.js
@@ -2,21 +2,62 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
+
+
+router.get('/:apiKey', (req, res) => {
 
     let queryText =
         `SELECT "crew_image" FROM "crew"
     ORDER BY "crew_id" DESC;`;
 
-    pool.query(queryText)
-        .then(results => {
-            res.send(results.rows);
+    const apiKeyCheckQuery = `SELECT * FROM "user" WHERE "api_key"=$1`;
+
+    pool.query(apiKeyCheckQuery, [req.params.apiKey])
+        .then((response) => {
+            if (response.rows.length == 1) {
+                pool.query(queryText)
+                    .then(results => {
+                        res.send(results.rows);
+                    })
+                    .catch(error => {
+                        console.log(`couldn't get mission info`, error);
+                        res.sendStatus(500);
+                    });
+            } else {
+                res.sendStatus(401);
+            }
         })
-        .catch(error => {
-            console.log(`couldn't get data`, error);
+        .catch((err) => {
+            console.warn(err);
+            res.sendStatus(500);
+        })
+});
+
+router.get('/:id/:apiKey', (req, res) => {
+
+    let queryText =
+        `SELECT "crew_image" FROM "crew"
+         WHERE "crew_id" = $1;`;
+
+const apiKeyCheckQuery = `SELECT * FROM "user" WHERE "api_key"=$1`;
+
+    pool.query(apiKeyCheckQuery, [req.params.apiKey])
+        .then((response) => {
+            if (response.rows.length == 1) {
+                pool.query(queryText, [req.params.id])
+                    .then(results => {
+                        res.send(results.rows);
+                    })
+                    .catch(error => {
+                        console.log(`couldn't get mission info`, error);
+                        res.sendStatus(500);
+                    });
+            } else {
+                res.sendStatus(401);
+            }
+        })
+        .catch((err) => {
+            console.warn(err);
             res.sendStatus(500);
         })
 });
