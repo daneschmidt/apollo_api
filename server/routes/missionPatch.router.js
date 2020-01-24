6dcb368b43d57ea70103d96afd7628df3ff24874
Dane Schmidt <daneschmidt@gmail.com>
2020-01-24 09:28:15 -0600

    missionPatch router updated to require API key to return data

--- server/routes/missionPatch.router.js
+++ server/routes/missionPatch.router.js
@@ -2,9 +2,7 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+
 router.get('/', (req, res) => {
     
     let queryText =
@@ -20,6 +18,64 @@ router.get('/', (req, res) => {
             res.sendStatus(500);
         })
 
+});
+
+router.get('/:apiKey', (req, res) => {
+
+    let queryText =
+        `SELECT "mission_patch" FROM "mission"
+        ORDER BY "crew_id" DESC;`;
+
+    const apiKeyCheckQuery = `SELECT * FROM "user" WHERE "api_key"=$1`;
+
+    pool.query(apiKeyCheckQuery, [req.params.apiKey])
+        .then((response) => {
+            if (response.rows.length == 1) {
+                pool.query(queryText)
+                    .then(results => {
+                        res.send(results.rows);
+                    })
+                    .catch(error => {
+                        console.log(`couldn't get mission info`, error);
+                        res.sendStatus(500);
+                    });
+            } else {
+                res.sendStatus(401);
+            }
+        })
+        .catch((err) => {
+            console.warn(err);
+            res.sendStatus(500);
+        })
+});
+
+router.get('/:id/:apiKey', (req, res) => {
+
+    let queryText =
+        `SELECT "mission_patch" FROM "mission"
+        WHERE "crew_id" = $1;`;
+
+const apiKeyCheckQuery = `SELECT * FROM "user" WHERE "api_key"=$1`;
+
+    pool.query(apiKeyCheckQuery, [req.params.apiKey])
+        .then((response) => {
+            if (response.rows.length == 1) {
+                pool.query(queryText, [req.params.id])
+                    .then(results => {
+                        res.send(results.rows);
+                    })
+                    .catch(error => {
+                        console.log(`couldn't get mission info`, error);
+                        res.sendStatus(500);
+                    });
+            } else {
+                res.sendStatus(401);
+            }
+        })
+        .catch((err) => {
+            console.warn(err);
+            res.sendStatus(500);
+        })
 });
 
 module.exports = router;
