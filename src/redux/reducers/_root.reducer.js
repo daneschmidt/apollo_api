f4ec5d2dd8fc1e714b11f668347eeb9ecfce19d5
Dane Schmidt <daneschmidt@gmail.com>
2020-01-26 12:22:26 -0600

    code cleanup

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -5,16 +5,10 @@ import user from './user.reducer';
 import missionReducer from './mission.reducer';
 import specMissionReducer from '../reducers/specMission.reducer';
 
-// rootReducer is the primary reducer for our entire project
-// It bundles up all of the other reducers so our project can use them.
-// This is imported in index.js as rootSaga
-
-// Lets make a bigger object for our store, with the objects from our reducers.
-// This is what we get when we use 'state' inside of 'mapStateToProps'
 const rootReducer = combineReducers({
-  errors, // contains registrationMessage and loginMessage
-  loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
-  user, // will have an id and username if someone is logged in
+  errors, 
+  loginMode, 
+  user, 
   missionReducer,
   specMissionReducer
 });
